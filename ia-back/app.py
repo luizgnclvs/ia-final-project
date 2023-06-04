db1364a5988d76b2c20425c8b4d2db34d3ce4b89
Luiz Gonçalves <94024386+luizgnclvs@users.noreply.github.com>
2023-06-04 19:33:07 -0300

    Update app.py

--- ia-back/app.py
+++ ia-back/app.py
@@ -3,7 +3,6 @@ from PIL import Image
 import os
 import numpy as np
 from tensorflow.keras.models import load_model
-from tensorflow.keras.preprocessing import image
 from tensorflow.keras.applications.vgg16 import preprocess_input
 
 app = Flask(__name__)
@@ -18,36 +17,32 @@ else:
 model = load_model(path, compile=False)
 model.compile()
 
-def preprocess(img):
-    img_array = image.img_to_array(img)
-    img_array = np.expand_dims(img_array, axis=0)
-    img_array /= 255.
-    return img_array
-    # img_resized = np.resize(img_array, (224, 224, 3))
-    # img_expanded = np.expand_dims(img_resized, axis=0)
-    # return preprocess_input(img_expanded)
+def preprocess(img_array):
+    img_resized = np.resize(img_array, (224, 224, 3))
+    img_expanded = np.expand_dims(img_resized, axis=0)
+    return preprocess_input(img_expanded)
 
 def get_class_info(prediction):
-    class_id = int(np.argmax(prediction))
+    type = int(np.argmax(prediction))
 
-    if class_id == 0:
-        class_name = "Calça"
-    elif class_id == 1:
-        class_name = "Camisa"
+    if type == 0:
+        name = "Calça"
+    elif type == 1:
+        name = "Camisa"
     else:
-        class_name = "Sapato"
+        name = "Sapato"
     
-    return class_id, class_name
+    return type, name
 
 @app.route('/uploads', methods=['POST'])
 def predict():
     file = request.files['file']
     img = Image.open(file.stream)
-    # img_array = np.array(img)
-    img_processed = preprocess(img)
+    img_array = np.array(img)
+    img_processed = preprocess(img_array)
     prediction = model.predict(img_processed)
-    class_id, class_name = get_class_info(prediction)
-    response = {'class_id': class_id, 'class_name': class_name}
+    type, name = get_class_info(prediction)
+    response = { 'type': type, 'name': name }
     return jsonify(response)
 
 if __name__ == '__main__':
