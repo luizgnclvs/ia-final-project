dbde75ffc7221ab8a2b87805be653a946ead302a
luizgnclvs <luizgnclvss@gmail.com>
2023-06-04 22:10:47 -0300

    Alteração no pré-processamento de imagens

--- ia-back/app.py
+++ ia-back/app.py
@@ -1,11 +1,15 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 from PIL import Image
 import os
 import numpy as np
 from tensorflow.keras.models import load_model
 from tensorflow.keras.applications.vgg16 import preprocess_input
+from tensorflow.keras.preprocessing import image
 
 app = Flask(__name__)
+CORS(app)
+
 UPLOAD_FOLDER = 'uploads/'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
@@ -17,13 +21,15 @@ else:
 model = load_model(path, compile=False)
 model.compile()
 
-def preprocess(img_array):
-    img_resized = np.resize(img_array, (224, 224, 3))
-    img_expanded = np.expand_dims(img_resized, axis=0)
-    return preprocess_input(img_expanded)
+def preprocess(img):
+    img = img.resize((224, 224))
+    img_array = image.img_to_array(img)
+    img_array = np.expand_dims(img_array, axis=0)
+    img_array /= 255.
+    return img_array
 
 def get_class_info(prediction):
-    class_id = int(np.argmax(prediction))
+    class_id = np.argmax(prediction)
 
     if class_id == 0:
         class_name = "Calça"
@@ -32,14 +38,13 @@ def get_class_info(prediction):
     else:
         class_name = "Sapato"
     
-    return class_id, class_name
+    return int(class_id), class_name
 
 @app.route('/uploads', methods=['POST'])
 def predict():
     file = request.files['file']
     img = Image.open(file.stream)
-    img_array = np.array(img)
-    img_processed = preprocess(img_array)
+    img_processed = preprocess(img)
     prediction = model.predict(img_processed)
     class_id, class_name = get_class_info(prediction)
     response = { 'type': class_id, 'name': class_name }
